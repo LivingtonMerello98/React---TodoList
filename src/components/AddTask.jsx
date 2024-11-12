e77593616a2f558f0d97e00a48cb7ff9afc4aa45
LivingtonMerello98 <livington.merello@gmail.com>
2024-11-12 11:49:43 +0100

    [UPD] select field in addtask

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,32 +1,44 @@
-// src/components/AddTask.jsx
 import React, { useState } from 'react';
+import '../App.css';
 
 function AddTask({ onAddTask }) {
     const [task, setTask] = useState('');
+    const [category, setCategory] = useState('lavoro'); // Stato per la categoria selezionata
 
     const handleSubmit = (e) => {
-
         e.preventDefault();
-        //il metodo trim() rimuove gli spazi
         if (task.trim()) {
-            console.log(`segnaposto ${task.trim()}`)
-            onAddTask(task);
+            // Passiamo la task e la categoria all'oggetto della nuova task
+            onAddTask({ task: task.trim(), category });
 
             setTask('');
-
         }
     };
 
     return (
-        <form onSubmit={handleSubmit} className="flex items-center space-x-2 mt-4 w-6/12 bg-blue-300">
+        <form onSubmit={handleSubmit} className="flex items-center space-x-2 mt-4 w-full">
             <input
                 type="text"
                 value={task}
                 onChange={(e) => setTask(e.target.value)}
-                className="flex-1 p-2 border rounded"
+                className="flex-1 py-1 px-2 border rounded bg-gray-500 input-class"
                 placeholder="Add a new task"
             />
-            <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
+
+            <select
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                className="py-1 px-2 border rounded bg-gray-500 border-gray-600 text-white select"
+            >
+                <option value="lavoro">Lavoro</option>
+                <option value="svago">Svago</option>
+                <option value="esercizio">Esercizio</option>
+            </select>
+
+            <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded text-xs"
+            >
                 Add
             </button>
         </form>

4ade6eaa647ba5b581a1a38837e7604d23033080
LivingtonMerello98 <livington.merello@gmail.com>
2024-11-29 03:13:04 +0100

    [Fix] LIst component

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -19,41 +19,37 @@ function AddTask() {
     };
 
     return (
-        <form
-            onSubmit={handleSubmit}
-            className="flex flex-col sm:flex-row md:flex-row items-center space-y-4 sm:space-y-0 md:space-x-4 mt-4 w-full"
-        >
+        <form onSubmit={handleSubmit} className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 mt-4 w-full">
             <input
                 type="text"
                 value={task}
                 onChange={(e) => setTask(e.target.value)}
-                className="w-full sm:w-auto md:flex-1 py-2 px-2 border border-white rounded input-class text-sm"
+                className="w-full md:w-auto flex-1 py-2 px-2 border border-white rounded input-class text-sm"
                 placeholder="Add a new task"
             />
-            <select
-                value={category}
-                onChange={(e) => setCategory(e.target.value)}
-                className="select py-2 px-2 border rounded text-white w-full sm:w-48"
-            >
-                <option value="work">Work</option>
-                <option value="fun">Fun</option>
-                <option value="personal">Personal</option>
-            </select>
+            <div className="flex flex-col sm:flex-row gap-2 w-full md:w-auto">
+                <select
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    className="select py-2 px-2 border rounded text-white w-full sm:w-48"
+                >
+                    <option value="work">Work</option>
+                    <option value="fun">Fun</option>
+                    <option value="personal">Personal</option>
+                </select>
 
-            <select
-                value={time}
-                onChange={(e) => setTime(e.target.value)}
-                className="select py-2 px-2 border rounded text-white w-full sm:w-48"
-            >
-                <option value="morning">Morning</option>
-                <option value="afternoon">Afternoon</option>
-                <option value="evening">Evening</option>
-            </select>
+                <select
+                    value={time}
+                    onChange={(e) => setTime(e.target.value)}
+                    className="select py-2 px-2 border rounded text-white w-full sm:w-48"
+                >
+                    <option value="morning">Morning</option>
+                    <option value="afternoon">Afternoon</option>
+                    <option value="evening">Evening</option>
+                </select>
+            </div>
 
-            <button
-                type="submit"
-                className="w-full sm:w-auto md:w-auto bg-white text-blue-900 px-4 py-2 rounded text-xs font-bold"
-            >
+            <button type="submit" className="w-full md:w-auto bg-white text-blue-900 px-4 py-2 rounded text-xs mt-4 sm:mt-0 font-bold">
                 <FontAwesomeIcon icon="plus" />
             </button>
         </form>
